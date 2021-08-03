ab5f3982e1bb967f405f7742696f0bc64a571040
plumcot <410530506@qq.com>
2021-08-03 16:01:29 +0000

    Deploying to gh-pages from  @ 552d031a06b025bd3ded4d2e3022d31d48e82ff4 🚀

---
+++ .vuepress/config/nav/index.js
@@ -0,0 +1,23 @@
+module.exports = [
+    {
+        "text": "主页",
+        "link": "/",
+        "icon": "reco-home"
+    },
+    {
+        "text": "简历",
+        "link": "https://www.500d.me/cvresume/9774192035/",
+        // "link": "http://resume.houcy7.com",
+        "icon": "reco-coding"
+    },
+    {
+        "text": "时间轴",
+        "link": "/timeline/",
+        "icon": "reco-date"
+    },
+    {
+        "text": "关于",
+        "link": "/about/",
+        "icon": "reco-account"
+    }
+];
